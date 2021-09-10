da83bd9a1953760e94140b47e3bd6f255b6f1d9d
Ariadine Gomes <ariadine@funda.nl>
2021-09-10 14:09:09 +0200

    chore(addon-postcss): implicit PostCSS loader is deprecated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
     stories: ['../src/**/*.stories.js'],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-postcss'],
 };
